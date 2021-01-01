8356a61d71a53272a73ba9e3c9323f19c7695289
Luke Becker <luke.w.becker32@gmail.com>
2020-12-31 21:17:46 -0600

    practicing classes in python

--- personal-projects/python-crash-course/classes_practice_exercises.py
+++ personal-projects/python-crash-course/classes_practice_exercises.py
@@ -24,3 +24,16 @@ my_dog = Dog('Willie', 6)
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age} years old.")
 
+# class example dot notation:
+
+my_dog = Dog("Wille", 6)
+your_dog = Dog("Lucy", 3)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
+
